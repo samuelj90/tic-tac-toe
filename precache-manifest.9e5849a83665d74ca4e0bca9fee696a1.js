703849bd56d1be434c97b7cb6682dee4b651437c
Samuel J Mathew <samuelj90@gmail.com>
2020-05-08 18:38:50 +0530

    Adding dist folder to source repo

---
+++ precache-manifest.9e5849a83665d74ca4e0bca9fee696a1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d6bae0989860bb7e69c057eab5b3dd14",
+    "url": "/index.html"
+  },
+  {
+    "revision": "daa2d2ae650fccf783b3",
+    "url": "/static/css/main.45166c3f.chunk.css"
+  },
+  {
+    "revision": "6462b522d33a4314f15b",
+    "url": "/static/js/2.e3a62433.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.e3a62433.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "daa2d2ae650fccf783b3",
+    "url": "/static/js/main.5c7bbc78.chunk.js"
+  },
+  {
+    "revision": "2893c4269a291b1fc016",
+    "url": "/static/js/runtime-main.5c177eb3.js"
+  }
+]);
